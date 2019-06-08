1ab52723d263e7c59cef1ec4d89284bcaf1aac4a
Marco Mannucci <markomannux@gmail.com>
2019-06-08 07:22:09 +0200

    Aggiunto service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,64 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
-var path = require('path');
+const path = require('path');
 module.exports = {
-    entry: './src/index.js',
-    devServer: {
-      contentBase: './dist'
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-              test: /\.css$/,
-              use: [
-                'style-loader',
-                'css-loader'
-              ]
-            },
-            {
-              test: path.join(__dirname, 'src/es6'),
-              use: 'babel-loader'
-            },
-            {
-              test: /\.(png|svg|jpg|gif)$/,
-              use: [
-                'file-loader'
-              ]
-            }
-        ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: "Salta salta freccia",
-        template: "./src/index.html"
-      })
-    ]
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: path.join(__dirname, 'src/es6'),
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Salta salta freccia',
+      template: './src/index.html',
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.join(__dirname, 'src/sw.js'),
+    }),
+    new WebpackPwaManifest({
+      'name': 'Salta Salta Freccia',
+      'short_name': 'SSF',
+      'description': 'Description!',
+      'background_color': '#01579b',
+      'theme_color': '#01579b',
+      'theme-color': '#01579b',
+      'start_url': '/',
+      'display': 'fullscreen',
+      'orientation': 'landscape',
+      'icons': [
+        {
+          src: path.resolve('src/assets/images/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
 };
